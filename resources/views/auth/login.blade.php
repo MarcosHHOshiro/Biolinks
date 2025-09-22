35008e2cc92849f160de171d4d59e21d7e5f6461
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-09-22 16:57:55 -0400

    Iniciando configs do blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,35 @@
-<div>
-    {{ auth()->id() }}
-
-    <h1>Login</h1>
-
-    @if ($message = session()->get('message'))
-        <div>
-            {{ session('message') }}
-        </div>
-    @endif
-
+<x-layout.app>
     <div>
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
+        {{ auth()->id() }}
 
+        <h1>Login</h1>
+
+        @if ($message = session()->get('message'))
             <div>
-                <input type="text" name="email" placeholder="Email" value="{{ old('email') }}">
-                @error('email')
-                    <span>{{ $message }}</span>
-                @enderror
+                {{ session('message') }}
             </div>
+        @endif
 
-            <div>
-                <input type="password" name="password" placeholder="Password">
-                @error('password')
-                    <span>{{ $message }}</span>
-                @enderror
-            </div>
+        <div>
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
 
-            <button type="submit">Login</button>
-        </form>
+                <div>
+                    <input type="text" name="email" placeholder="Email" value="{{ old('email') }}">
+                    @error('email')
+                        <span>{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div>
+                    <input type="password" name="password" placeholder="Password">
+                    @error('password')
+                        <span>{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </div>
-</div>
+</x-layout.app>
---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+
+    <title> {{ config('app.name') }} </title>
+</head>
+
+<body class="bg-slate-900 text-slate-50">
+    {{ $slot }}
+</body>
+
+</html>
